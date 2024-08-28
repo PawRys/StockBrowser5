42932928cb92e417e87a850c0f422f5ae4c50e0e
PawRys <hellbu@gmail.com>
2024-08-28 19:58:53 +0200

    other things

--- src/exports/urls.ts
+++ src/exports/urls.ts
@@ -1,2 +1,2 @@
-// export const php_server_url = 'https://bossman.hekko24.pl/stock_browser_server/server.php'
-export const php_server_url = 'http://localhost:3000/php_server/server.php'
+export const php_server_url = 'https://bossman.hekko24.pl/stock_browser_server/server.php'
+// export const php_server_url = 'http://localhost:3000/php_server/server.php'
