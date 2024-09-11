dad93c260c5231ec3618effcc059f50870985e89
PawRys-Smetek <hellbu@gmail.com>
2024-09-11 13:25:43 +0200

    fix database stats update

--- src/components/StockBrowser/DataStats.vue
+++ src/components/StockBrowser/DataStats.vue
@@ -5,10 +5,10 @@ import { useStockStore } from '@/stores/stockStore'
 import { hasPurchase } from '@/exports/common_functions'
 // import {  hasReservations } from '@/exports/common_functions'
 
-const { date, warehause } = storeToRefs(useStockStore())
+const { date, items, warehause } = storeToRefs(useStockStore())
 const refreshLocalComponent = ref(0)
 
-watch([date, warehause], () => {
+watch([date, items, warehause], () => {
   refreshLocalComponent.value++
 })
 
