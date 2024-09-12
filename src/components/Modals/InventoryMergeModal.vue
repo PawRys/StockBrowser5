fecf52ddb10399fc81503d88e8e036231729a85c
PawRys-Smetek <hellbu@gmail.com>
2024-09-12 08:02:19 +0200

    changed modal

--- src/components/Modals/InventoryMergeModal.vue
+++ src/components/Modals/InventoryMergeModal.vue
@@ -9,25 +9,38 @@ import { Modal } from 'jenesius-vue-modal'
 
       <p>W ściąganych danych znajdują się wpisy inwentaryzacji. Co robić?</p>
 
-      <button @click="$emit(Modal.EVENT_PROMPT, 'local')" class="btn">
-        <i class="bi bi-pc-display-horizontal"></i>
-        <span><b>Pozostaw</b> bez zmian</span>
-      </button>
+      <div>
+        <button @click="$emit(Modal.EVENT_PROMPT, 'local')" class="btn">
+          <!-- <i class="bi bi-pc-display-horizontal"></i> -->
+          <span><b>Pozostaw</b> bez zmian</span>
+          <i class="bi bi-x-square"></i>
+        </button>
+      </div>
 
-      <button @click="$emit(Modal.EVENT_PROMPT, 'cloud')" class="btn">
-        <i class="bi bi-cloud-arrow-down-fill"></i>
-        <span><b>Zamień</b> na ściągane dane</span>
-      </button>
+      <div>
+        <button @click="$emit(Modal.EVENT_PROMPT, 'cloud')" class="btn">
+          <!-- <i class="bi bi-cloud-arrow-down-fill"></i> -->
+          <span><b>Zamień</b> na ściągane dane</span>
+          <i class="bi bi-arrow-clockwise"></i>
+        </button>
+      </div>
 
-      <button @click="$emit(Modal.EVENT_PROMPT, 'merge')" class="btn">
-        <i class="bi bi-pc-display-horizontal"></i> + <i class="bi bi-cloud-arrow-down-fill"></i>
-        <span><b>Sumuj</b></span>
-      </button>
+      <div>
+        <button @click="$emit(Modal.EVENT_PROMPT, 'merge')" class="btn">
+          <!-- <i class="bi bi-pc-display-horizontal"></i> + <i class="bi bi-cloud-arrow-down-fill"></i> -->
+          <span><b>Sumuj</b></span>
+          <i class="bi bi-plus-square"></i>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.dialog {
+  display: block;
+}
+
 .btn {
   flex-wrap: wrap;
   margin: 10px;
