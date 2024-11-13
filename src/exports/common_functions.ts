6ba3df1b0e770cdfcc3b5f39dcbc7d570088a63a
PawRys-Smetek <hellbu@gmail.com>
2024-11-13 12:02:05 +0100

    fix

--- src/exports/common_functions.ts
+++ src/exports/common_functions.ts
@@ -75,13 +75,14 @@ export function scrollTo(element: string, pxOffset: number) {
 
 export function reduceExpr(expr: string): string {
   expr = expr.replace(/,/gi, '.')
-  expr = expr.replace(/[^-+*/.0-9()@#$%&=]/gi, '')
-  while (/--|\+\+|-\+|\+-/.test(expr)) {
+  while (/(--|-\+|\+-)/.test(expr)) {
     expr = expr.replace(/-\+|\+-/, '-')
-    expr = expr.replace(/--|\+\+/, '+')
+    expr = expr.replace(/--/, '+')
+    // expr = expr.replace(/ {2,}/, ' ')
   }
+  expr = expr.replace(/[^-+*/.0-9()@#$%&=]/gi, '')
   // expr = expr.replace(/([-+])\b/gi, '$1\u200B') //zero width space
-  expr = expr.replace(/([-+])\b/gi, ' $1') //zero width space
+  // expr = expr.replace(/\b([-+])/gi, ' $1') //zero width space
   expr = expr.replace(/\B(\.)/gi, '0$1')
   expr = expr.replace(/(\d)(\()/gi, '$1*$2')
   expr = expr.replace(/(\))(\d)/gi, '$1*$2')
