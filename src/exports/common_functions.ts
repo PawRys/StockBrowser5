0be6de61dfc0747845e85d2b5e84f4db6c9c9d43
PawRys-Smetek <hellbu@gmail.com>
2026-04-17 13:24:25 +0200

    better evalMath

--- src/exports/common_functions.ts
+++ src/exports/common_functions.ts
@@ -81,11 +81,12 @@ export function prettierExpression(expr: string): string {
     expr = expr.replace(/--|\+\+/, '+')
     expr = expr.replace(/[/*]([/*])/, '$1')
   }
-  expr = expr.replace(/\B(\.)/gi, '0$1')
+  expr = expr.replace(/(^|\D)([.][0-9])/gi, '$10$2')
   expr = expr.replace(/([0-9])([(])/gi, '$1*$2')
   expr = expr.replace(/([)])([0-9])/gi, '$1*$2')
   expr = expr.replace(/([)])([(])/gi, '$1*$2')
-  expr = expr.replace(/([0-9]) ([0-9])/gi, '$1$2')
+  expr = expr.replace(/([0-9])[ ]+([0-9])/gi, '$1$2')
+  expr = expr.replace(/[(]$/gi, '(1')
   return expr
 }
 
